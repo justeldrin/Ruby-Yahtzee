ba2a55e7d93f2be7950d6f5963d9339942a31e51
Tyler Yamaguchi <tmyamaguchi@cpp.edu>
2021-11-21 16:27:12 -0800

    Started adding score table

--- yahtzee.rb
+++ yahtzee.rb
@@ -86,7 +86,6 @@ set title: 'Yahtzee', background: '#006400', width: 700, height: 500
                 end
             end
         end
-
         def getScore
             
             return @score
@@ -94,26 +93,58 @@ set title: 'Yahtzee', background: '#006400', width: 700, height: 500
 
     end
 
-    
-    di = Array.new(6)
-    
+    class Score
+        def initialize()
+            @val = 0
+            @selected = false
+        end
+
+        def setVal(i)
+            @val = i
+        end
+        
+        def select
+            @selected = true
+        end
+
+        def getSelected
+            return @selected
+        end
+
+        def getVal
+            return @val
+        end
+    end
+
+    turn = 1
+    rerolls = 2
+    di = Array.new(5)
     # initiallize di with separate coordinates
     di[0] = Dice.new(50, 50)
     di[1] = Dice.new(100, 50)
     di[2] = Dice.new(150, 50)
     di[3] = Dice.new(200,50)
     di[4] = Dice.new(250, 50)
-    di[5] = Dice.new(300,50)
 
-    
+    sc = Array.new(18)
+    for i in 0..17 do
+        sc[i] = Score.new()
     # key down
     on :key_down do |event|
         case event.key
             # roll button = R key
             when 'r'
-                for i in 0..5 do
-                    di[i].roll
+                if (rerolls > 0)
+                    for i in 0..4 do
+                        di[i].roll
+                    end
+                    rerolls -= 1
                 end
+                for i in 0..16 do
+                    chooseCalc(i, di, scoreArr)
+                end
+            
+
             # hold dice = corresponding number key
             when '1'
                 di[0].hold
@@ -125,8 +156,186 @@ set title: 'Yahtzee', background: '#006400', width: 700, height: 500
                 di[3].hold
             when '5'
                 di[4].hold
-            when '6'
-                di[5].hold
+            when 'a'
+                chooseCalc(0, di, sc)
+                if(!sc[0].getSelected)
+                    nextTurn()
+                end
+                
         end
     end
+    
+    def chooseCalc(choice, die, scoreArr)
+        if(!sc[choice].getSelected)
+            case choice
+                when '0'#aces
+                    sc[0].setVal(upperSums(1, die))
+                when '1'#twos
+                    sc[1].setVal(upperSums(2, die))
+                when '2'#threes
+                    sc[2].setVal(upperSums(3, die))
+                when '3'#fours
+                    sc[3].setVal(upperSums(4, die))
+                when '4'#fives
+                    sc[4].setVal(upperSums(5, die))
+                when '5'#sixes
+                    sc[5].setVal(upperSums(6, die))
+                when '6'#Num total
+                    sc[6].setVal(numTotal(sc))
+                when '7'#Bonus
+                    if(sc[6].getVal >= 65)
+                        sc[7].setVal(35)
+                    else
+                        sc[7].setVal(0)
+                    end
+                when '8'#uppertotal
+                    sc[8].setVal(sc[6].getVal + sc[7].getVal)
+                when '9'#three of a kind
+                    sc[9].setVal(threeKind(die))
+                when '10'#four of a kind
+                    sc[10].setVal(fourKind(die))
+                when '11'#Yachzee - five of a kind
+                    sc[11].setVal(yahtzee(die))
+                when '12'# Full house
+                    sc[12].setVal(fullHouse(die))
+                when '13'#straight of 4
+                    sc.[13].setVal(smallStraight(die))
+                when '14'#straight of 5
+                    sc.[14].setVal(largeStraight(die))
+                when '15'#chance
+                    sc.[15].setVal(chance(die))
+                when '16'#low total
+                    sc.[16].setVal(lowTotal(sc))
+                when '17'#Grand total
+                    sc.[17].setVal(sc[16].getVal + sc[8].getVal)
+            end
+        end
+    end
+
+    def nextTurn()
+        if(turn != 13)
+            turn += 1
+            rerolls = 2
+        else
+            sc[17] = grandTotal(di)
+        end
+    end
+
+    def numTotal(scoreArr)
+        sum = 0
+        for i in 0..5
+            sum += scoreArr[i].getVal
+        end
+    end
+
+    def lowTotal(scoreArr)
+    sum = 0
+        for i in 9..15
+            sum += scoreArr[i].getVal
+        end
+    end
+    
+    #Table UI
+    tableImage = Image.new('scoreTable.png',
+        x:480, y:0
+        )
+    
+        acesText = Text.new('',
+            x: 635, y: 26,
+            color: 'black'
+        )
+    
+        twosText = Text.new('',
+            x: 635, y: 48,
+            color: 'black'
+        )
+    
+        
+        threesText = Text.new('',
+            x: 635, y: 70,
+            color: 'black'
+        )
+        
+        foursText = Text.new('',
+            x: 635, y: 92,
+            color: 'black'
+        )
+    
+        
+        fivesText = Text.new('',
+            x: 635, y: 114,
+            color: 'black'
+        )
+    
+        
+        sixesText = Text.new('',
+            x: 635, y: 136,
+            color: 'black'
+        )
+    
+        upperTotalScoreText = Text.new('',
+            x: 635, y: 158,
+            color: 'black'
+        )
+        
+        bonusText = Text.new('',
+            x: 635, y: 180,
+            color: 'black'
+        )
+    
+        upperTotalText = Text.new('',
+            x: 635, y: 204,
+            color: 'black'
+        )
+    
+        
+        threeKindText = Text.new('',
+            x: 635, y: 272,
+            color: 'black'
+        )
+    
+        fourKindText = Text.new('',
+            x: 635, y: 294,
+            color: 'black'
+        )
+    
+        fiveKindText = Text.new('',
+            x: 635, y: 316,
+            color: 'black'
+        )
+    
+        fullHouseText = Text.new('',
+            x: 635, y: 338,
+            color: 'black'
+        )
+    
+        fourStraightText = Text.new('',
+            x: 635, y: 360,
+            color: 'black'
+        )
+    
+        fiveStraightText = Text.new('',
+            x: 635, y: 382,
+            color: 'black'
+        )
+    
+        chanceText = Text.new('',
+            x: 635, y: 404,
+            color: 'black'
+        )
+    
+        grandLowerText = Text.new('',
+            x: 635, y: 430,
+            color: 'black'
+        )
+    
+        grandUpperText = Text.new('',
+            x: 635, y: 452,
+            color: 'black'
+        )
+    
+        grandTotalText = Text.new('',
+            x: 635, y: 474,
+            color: 'black'
+        )
 show
